6ff3227bbe28a344167121a80b5e30dc2a91edae
Dmytro Melnychenko <51658169+bee7ch7@users.noreply.github.com>
2024-06-19 20:48:50 -0600

    update outputs

--- aws/aws_sagemaker/modules/endpoints/outputs.tf
+++ aws/aws_sagemaker/modules/endpoints/outputs.tf
@@ -9,11 +9,11 @@ output "endpoint_name" {
 }
 
 output "endpoint_configuration_arn" {
-  value       = try(aws_sagemaker_endpoint.this[0].arn, "")
+  value       = try(aws_sagemaker_endpoint_configuration.this[0].arn, "")
   description = "Endpoint configuration ARN"
 }
 
 output "endpoint_configuration_name" {
-  value       = try(aws_sagemaker_endpoint.this[0].name, "")
+  value       = try(aws_sagemaker_endpoint_configuration.this[0].name, "")
   description = "Endpoint configuration name"
 }
